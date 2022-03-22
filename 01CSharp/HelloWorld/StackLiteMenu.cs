bed38b1cdba33aa658b3e6d215357bbc53a5815e
Juniper Song <songminseonk@gmail.com>
2022-03-22 15:36:20 -0400

    basic stackLite demo

--- 01CSharp/HelloWorld/StackLiteMenu.cs
+++ 01CSharp/HelloWorld/StackLiteMenu.cs
@@ -14,48 +14,98 @@ public class StackLiteMenu
     //method defines a behavior of a class
     public void MainMenu()
     {
+        List<Issue> issues = new List<Issue>();
+        // List<int> intList = new List<int>();
+        // List<string> stringList = new List<string>();
+        // List<bool> boolList = new List<bool>();
+        // List<List<Issue>> listofListofIssues = new List<List<Issue>>();
+        // listofListofIssues.Add(new List<Issue>() {
+        //     new Issue("title", "content"),
+        //     new Issue("another", "one"),
+        //     new Issue("third", "one")
+        // });
+        // listofListofIssues.Add(new List<Issue>() {
+        //     new Issue("2title", "2content"),
+        //     new Issue("another", "one")
+        // });
         Console.WriteLine("Welcome to StackLite!");
-        Console.WriteLine("What brings you here today?");
+        bool exit = false;
+        do
+        {
+            EnterTitle:
+            Console.WriteLine("What brings you here today?");
 
-        // string? input = Console.ReadLine();
+            // string? input = Console.ReadLine();
 
-        // if(input == null) {
-        //     Console.WriteLine("you didn't type anything!");
-        // }
+            // if(input == null) {
+            //     Console.WriteLine("you didn't type anything!");
+            // }
+            
+            //null coalescing operator (??)
+            string title = Console.ReadLine() ?? "";
+            if(String.IsNullOrWhiteSpace(title)) {
+                Console.WriteLine("Your title cannot be empty");
+                goto EnterTitle;
+            }
+            
+            //Some Examples of Value types in C#
+            // int n = 10; // is Int32
+            // //or true
+            // bool boolean = false;
+            // double number = 0.00;
+            // float floatNum = 0.00f;
+            // decimal decimalNumber = 0.00M;
+            // long longNumber = 100000L; //Long is Int64
+            // short shortNumber = new short();
 
-        EnterTitle:
-        //null coalescing operator (??)
-        string title = Console.ReadLine() ?? "";
-        if(String.IsNullOrWhiteSpace(title)) {
-            Console.WriteLine("Your title cannot be empty");
-            goto EnterTitle;
-        }
+            EnterBody:
+            Console.WriteLine("Tell us more about your issue");
+            string content = Console.ReadLine() ?? "";
+
+            if(String.IsNullOrWhiteSpace(content)) {
+                Console.WriteLine("You can't have your body be empty");
+                goto EnterBody;
+            }
+
+            // Issue newIssue = new Issue();
+            // newIssue.Title = title;
+            // newIssue.Content = content;
+            // newIssue.Score = 0;
+
+            Issue createdIssue = new Issue(title, content);
+            issues.Add(createdIssue);
+            
+            // for(int i = 0; i < issues.Count; i++)
+            // {
+            //     Console.WriteLine(issues[i]);
+            // }
+
+            foreach(Issue issueToDisplay in issues)
+            {
+                Console.WriteLine(issueToDisplay);
+            }
+
+            Another:
+            Console.WriteLine("Do you want to enter another? [Y/N]");
+            string enterAnother = Console.ReadLine() ?? "";
+            if(String.IsNullOrWhiteSpace(enterAnother))
+            {
+                Console.WriteLine("Please enter valid input");
+                goto Another;
+            }
+            char responseChar = enterAnother.Trim().ToUpper()[0];
+
+            if(responseChar == 'N')
+            {
+                Console.WriteLine("Goodbye!");
+                exit = true;
+            }
+            else if(responseChar != 'Y')
+            {
+                Console.WriteLine("Please enter valid response");
+                goto Another;
+            }
+        }while(!exit);
         
-        //Some Examples of Value types in C#
-        // int n = 10; // is Int32
-        // //or true
-        // bool boolean = false;
-        // double number = 0.00;
-        // float floatNum = 0.00f;
-        // decimal decimalNumber = 0.00M;
-        // long longNumber = 100000L; //Long is Int64
-        // short shortNumber = new short();
-
-        EnterBody:
-        Console.WriteLine("Tell us more about your issue");
-        string content = Console.ReadLine() ?? "";
-
-        if(String.IsNullOrWhiteSpace(content)) {
-            Console.WriteLine("You can't have your body be empty");
-            goto EnterBody;
-        }
-
-        // Issue newIssue = new Issue();
-        // newIssue.Title = title;
-        // newIssue.Content = content;
-        // newIssue.Score = 0;
-
-        Issue createdIssue = new Issue(title, content);
-        Console.WriteLine("You entered " + createdIssue.ToString());
     }
 }
