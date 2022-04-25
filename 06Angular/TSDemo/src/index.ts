e17548b0136d12ed66864f2a610898fa0de99b3c
Juniper Song <songminseonk@gmail.com>
2022-04-25 16:40:05 -0400

    comments

--- 06Angular/TSDemo/src/index.ts
+++ 06Angular/TSDemo/src/index.ts
@@ -2,24 +2,28 @@ import { issue } from './issue'
 import { answer } from './answer'
 
 export function submitIssue(event : Event) : void {
-    //I want to grab all data from html forms,
+    //I want to grab all data from html form,
     //and make a post request to the /api/issues end point
     event.preventDefault()
     console.log('submitting a new question..')
 
+    /**
+     * grabbing form element by its id
+     * and type casting to table element to gain access to table related methods
+     */
     let form = document.getElementById('new-question-form') as HTMLFormElement;
     let title = document.getElementById('q-title') as HTMLInputElement;
     let content = document.getElementById('q-content') as HTMLTextAreaElement;
 
+    //Utility class to declare that i'm only using part of this class
     let newQuestion : Partial<issue> = {
         Title: title.value,
         Content: content.value,
         DateCreated: new Date(),
         Score: 0
     }
-
-    console.log(newQuestion);
     
+    //sending POST call to our api to create new question
     fetch('https://stackliteapi.azurewebsites.net/api/Issues', {
         method: 'POST',
         headers: {
@@ -27,26 +31,37 @@ export function submitIssue(event : Event) : void {
         },
         body: JSON.stringify(newQuestion)
     }).then(res => res.json()).then(resBody => {
-        console.log(resBody)
         alert('question added successfully!')
+        
+        //Reset the form
         title.value = '';
         content.value = '';
+
+        //navigate to "home page"
         window.location.pathname = ('06Angular/TSDemo/index.html')
     })
 }
 
+
+/**
+ * dynamically populating table from the data we get from be
+ */
 export function populateQuestionTable() : void {
+    //sending GET request
     fetch('https://stackliteapi.azurewebsites.net/api/Issues', {
         method: 'GET',
         headers: {
             'Access-Control-Allow-Origin': '*'
         }
     }).then((res) => res.json()).then(resbody => {
-        console.log(resbody)
+        // accessing the table element to populate with data
         let table : HTMLTableElement = document.getElementById('sl-table') as HTMLTableElement;
     
+        //get the table body
         let tbody : HTMLTableSectionElement = table.getElementsByTagName('tbody')[0];
     
+        //For each element in the response (aka issue objects)
+        //insert a new row
         resbody.forEach((element : issue) => {
             
             let tableRow : HTMLTableRowElement = tbody.insertRow(0);
@@ -56,25 +71,4 @@ export function populateQuestionTable() : void {
             tableRow.insertCell(3).innerHTML = element.Score.toString();
         });
     })
-
-    // let questionArr : issue[] = [
-    //     {
-    //         id: 1,
-    //         title: 'title',
-    //         score: 0,
-    //         content: 'content',
-    //         answers: [],
-    //         dateCreated: 'today'
-    //     },
-    //     {
-    //         id: 2,
-    //         title: 'title',
-    //         score: 0,
-    //         content: 'content',
-    //         answers: [],
-    //         dateCreated: 'yesterday'
-    //     }
-    // ]
-
-
 }
